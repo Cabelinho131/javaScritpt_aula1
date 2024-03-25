f7bf575568b097ba726b2193c959f5c1dbe25671
Cabelinho131 <161031808+Cabelinho131@users.noreply.github.com>
2024-03-25 11:39:33 -0300

    Add files via upload

---
+++ Nova pasta/script.js
@@ -0,0 +1,44 @@
+
+
+let lista = [1,2,3,4];
+ 
+
+ var greeter = "hey hi";
+    
+    function newFunction() {
+        var hello = "hello";
+    }
+    console.log(hello);
+
+    function newFunction()  {
+ let numero = 1;
+numero = ('1');}
+
+console.log(numero); // 1
+
+
+function newFunction()  {
+ const pessoas = {
+    nome: ['Diego, bruno ,joão'],
+    idade: 26
+  }
+  
+  
+  console.log.push(pessoas);
+
+  lista. push('joao, bruno');
+
+  console.log(pessoas.pop()); // "joão"
+
+  console.log(pessoas.shift());  // "diego"
+
+
+
+
+console.log(pessoas.unshift('pedro')); // "lucas"
+lista. unshift('pedro');
+
+console,log(lista.length);
+
+console.log('terminou');
+}
